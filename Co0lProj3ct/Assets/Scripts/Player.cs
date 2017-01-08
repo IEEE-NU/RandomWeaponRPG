91ee2249c273c7c7fa6334c379b97081f39251af
Murfalo <murphycangelo@u.northwestern.edu>
2017-01-07 18:39:47 -0600

    Add controllable red box "player"

---
+++ Co0lProj3ct/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	const float moveSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W))
+			transform.position += new Vector3(0, moveSpeed * Time.deltaTime);
+		if (Input.GetKey (KeyCode.A))
+			transform.position -= new Vector3(moveSpeed * Time.deltaTime, 0);
+		if (Input.GetKey (KeyCode.S))
+			transform.position -= new Vector3(0, moveSpeed * Time.deltaTime);
+		if (Input.GetKey (KeyCode.D))
+			transform.position += new Vector3(moveSpeed * Time.deltaTime, 0);
+	}
+}
