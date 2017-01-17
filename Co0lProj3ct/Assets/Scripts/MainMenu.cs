fc500a9d9d3823c2065d5d0de4e4bf1cbfa16cac
Rohaan Advani <RohaanAdvani68@gmail.com>
2017-01-17 15:30:39 -0600

    Added start screen

---
+++ Co0lProj3ct/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace AssemblyCSharp
+{
+	public class MainMenu:MonoBehaviour
+	{
+		public bool isStart;
+		public bool isQuit;
+		internal void OnMouseUp(){
+			if(isStart)
+			{
+				SceneManager.LoadScene("Main",LoadSceneMode.Single);
+			}
+			if (isQuit)
+			{
+				Application.Quit();
+			}
+		} 
+	}
+}
+
