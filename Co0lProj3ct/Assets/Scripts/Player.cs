c3aa5d5fb753aed927b373922320974700e4cb00
Rainy <chexunru06@sina.com>
2017-02-19 13:49:54 -0600

    Added sword

--- Co0lProj3ct/Assets/Scripts/Player.cs
+++ Co0lProj3ct/Assets/Scripts/Player.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 	const float moveSpeed = 5.0f;
+	public GameObject bullet;
 
 	// Use this for initialization
 	void Start () {
-		
+		bullet = Instantiate(Resources.Load<GameObject>("Prefabs/Bullet"));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//Movement 
 		if (Input.GetKey (KeyCode.W))
 			transform.position += new Vector3(0, moveSpeed * Time.deltaTime);
 		if (Input.GetKey (KeyCode.A))
@@ -21,22 +21,7 @@ public class Player : MonoBehaviour {
 			transform.position -= new Vector3(0, moveSpeed * Time.deltaTime);
 		if (Input.GetKey (KeyCode.D))
 			transform.position += new Vector3(moveSpeed * Time.deltaTime, 0);
-		//if(Input.GetKey(KeyCode.Space))
-			
-	}
-
-	void OnTriggerEnter2D(Collider2D obj){
-		if (obj.tag == "Ground") {
-			var rb = this.GetComponent<Rigidbody2D> ();
-			rb.velocity = new Vector3 (0, 0, 0);
-			rb.gravityScale = 0;
-		}
-	}
-
-	void OnTriggerExit2D(Collider2D obj){
-		if (obj.tag == "Ground") {
-			var rb = this.GetComponent<Rigidbody2D> ();
-			rb.gravityScale = 1;
-		}
+		if (Input.GetKey (KeyCode.L))
+			Instantiate (bullet, transform.position, transform.rotation);
 	}
 }
